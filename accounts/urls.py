16f23fc17dbd67e2ed9c00a4226937b934b30013
Thibault Jouannic <thibault@miximum.fr>
2013-05-23 08:41:47 +0200

    login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth.views import login, logout_then_login
 urlpatterns = patterns('accounts.views',
     url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', logout_then_login, name='logout'),
-    url(r'^forgot_password/$', 'send_password', name='send_password'),
+    url(r'^password_reset/$', 'password_reset', name='password_reset'),
 )
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from annoying.decorators import render_to
 
 
 @render_to('send_password.html')
-def send_password(self, request):
+def password_reset(self, request):
     """Login view using sms code."""
     return {}
 
