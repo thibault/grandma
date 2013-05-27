a260f3053cc838f35d0ce0cf5550616055b4526c
Thibault Jouannic <thibault@miximum.fr>
2013-05-27 12:00:33 +0200

    add phone to reminders model back

--- reminders/models.py
+++ reminders/models.py
@@ -6,13 +6,30 @@ from django.conf import settings
 
 
 class Reminder(models.Model):
-    user = models.ForeignKey(User, related_name='reminders')
+    user = models.ForeignKey(User, null=True, blank=True,
+                             related_name='reminders')
+    phone = models.CharField(_('Mobile'), max_length=20,
+                             null=True, blank=True,
+                             help_text=_('Use international format, e.g +33612345678'))
     message = models.CharField(_('Message'), max_length=150)
     when = models.DateTimeField(_('Date'))
     sent = models.BooleanField(_('Already sent?'), default=False)
 
+    class Meta:
+        verbose_name = _('Reminder')
+        verbose_name_plural = _('Reminders')
+
+    def get_phone(self):
+        """Get the actual phone to send the reminder."""
+        if self.user is not None:
+            phone = self.user.phone
+        else:
+            phone = self.phone
+        return phone
+
+
     def send(self):
         """Use the Nexmo API to send the reminder."""
-        send_message(self.user.phone, self.message)
+        send_message(self.get_phone(), self.message)
         self.sent = True
         self.save()
