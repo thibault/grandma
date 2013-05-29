b94c5dc44778e3590e338d2ee41d36dc1c4ebc95
Thibault Jouannic <thibault@miximum.fr>
2013-05-29 10:44:09 +0200

    payment form

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+import logging
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import redirect
@@ -8,19 +9,37 @@ from django.conf import settings
 from accounts.models import User
 
 
+logger = logging.getLogger(__name__)
+
 @render_to('register.html')
 def register(request):
     form = RegistrationForm(request.POST or None)
 
     if form.is_valid():
         token = request.POST['paymillToken']
-        py = pymill.Pymill(settings.PAYMILL_PRIVATE_KEY)
-        client = py.new_client(form.cleaned_data['email'])
-        card = py.new_card(token, client.id)
-        offer_id = settings.PAYMILL_OFFER_ID
-        subscription = py.new_subscription(client.id, offer_id, card.id)
 
-        # Store client_id, card_id, subscription_id
+        try:
+            # Let's request a payment with our provider
+            client = card = subscription = None
+            py = pymill.Pymill(settings.PAYMILL_PRIVATE_KEY)
+            client = py.new_client(form.cleaned_data['email'])
+            card = py.new_card(token, client.id)
+            offer_id = settings.PAYMILL_OFFER_ID
+            subscription = py.new_subscription(client.id, offer_id, card.id)
+        except Exception as e:
+            logger.error('Payment error token:%(token)s client:%(client)s '
+                         'card:%(card)s' % {
+                             'token': token,
+                             'client': client.id if client else 'NC',
+                             'card': card.id if card else 'NC'})
+            message = _('There was a problem processing your credit card. '
+                        'Your account was not created. Please, try again in '
+                        'a few minutes or with different payment informations.')
+            messages.error(request, message)
+            return { 'form': form }
+
+        # So payment was created, and form data is valid
+        # Let's create this user account
         data = form.cleaned_data
         user = User.objects.create_user(data['phone'], data['email'],
                                         paymill_client_id=client.id,
@@ -31,7 +50,6 @@ def register(request):
                     'your password by sms.')
         messages.success(request, message)
         return redirect('login')
-
 
     return {
         'form': form,
