c5a4c423dfd26f183b782b4aca20606a1fc54fda
Thibault Jouannic <thibault@miximum.fr>
2013-06-03 18:52:49 +0200

    don't check for valid user in send reminder task

--- reminders/management/commands/send_reminders.py
+++ reminders/management/commands/send_reminders.py
@@ -7,7 +7,6 @@ class Command(BaseCommand):
     """This tasks sends all the waiting reminders."""
     def handle(self, *args, **kwargs):
         reminders = Reminder.objects.filter(when__lte=timezone.now()) \
-                .filter(user__is_valid=True) \
                 .filter(sent=False)
 
         for reminder in reminders:
