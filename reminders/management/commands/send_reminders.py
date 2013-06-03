8a84931b79d59adc45f3de77ba3f726cce96e39a
Thibault Jouannic <thibault@miximum.fr>
2013-06-03 16:53:45 +0200

    use timezone in date queries

--- reminders/management/commands/send_reminders.py
+++ reminders/management/commands/send_reminders.py
@@ -1,4 +1,4 @@
-import datetime
+from django.utils import timezone
 from django.core.management.base import BaseCommand, CommandError
 from reminders.models import Reminder
 
@@ -6,7 +6,7 @@ from reminders.models import Reminder
 class Command(BaseCommand):
     """This tasks sends all the waiting reminders."""
     def handle(self, *args, **kwargs):
-        reminders = Reminder.objects.filter(when__lte=datetime.datetime.now) \
+        reminders = Reminder.objects.filter(when__lte=timezone.now()) \
                 .filter(user__is_valid=True) \
                 .filter(sent=False)
 
