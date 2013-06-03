c76ec296d0739ef3c07cabafb6ab494735985119
Thibault Jouannic <thibault@miximum.fr>
2013-06-03 15:48:36 +0200

    use phonefield in contact form

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from grandma.fields import PhoneField
 from contacts.models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    mobile = PhoneField(label=_('Phone'), required=True)
+
     class Meta:
         model = Contact
         exclude = ('user',)
