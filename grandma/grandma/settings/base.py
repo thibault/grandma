cb451179ef792a98b70f35142d814eb1313139c6
Thibault Jouannic <thibault@miximum.fr>
2013-07-16 17:03:16 +0200

    set login url in django settings

--- grandma/grandma/settings/base.py
+++ grandma/grandma/settings/base.py
@@ -25,6 +25,8 @@ DATABASES = {
     }
 }
 
+LOGIN_URL = '/account/login'
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
