7af3c4f30e8c5a2fddc7663973740b79a8fc4183
Thibault Jouannic <thibault@miximum.fr>
2014-01-07 16:41:09 +0100

    Add site root to path in wsgi file

--- src/grandma/wsgi.py
+++ src/grandma/wsgi.py
@@ -15,6 +15,9 @@ framework.
 """
 import os
 
+SITE_ROOT = dirname(dirname(abspath(__file__)))
+path.append(SITE_ROOT)
+
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
